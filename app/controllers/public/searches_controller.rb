39b76190c97e794ab15358d3c9761697380400ff
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2022-11-05 09:16:08 +0000

    [search]

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,13 @@
 class Public::SearchesController < ApplicationController
    def  search
      @range = params[:range]
+     unless params[:word].present?
+       @users = []
+       @posts = []
+       @range = []
+       return
+     end
+     
      if @range =="User"
        @users = User.looks(params[:word])
      elsif @range == "Post"
